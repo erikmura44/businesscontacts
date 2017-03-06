a25dc2910e8b6ae6a2f95bee79cc67ebf0e2027c
erikmura44 <erikmura44@gmail.com>
2017-03-06 14:08:51 -0700

    more info working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDcRreRW2PZ_Zd4mSmJa7i7wNV5rhnupVg",
+  authDomain: "businesscontacts-77be3.firebaseapp.com",
+  databaseURL: "https://businesscontacts-77be3.firebaseio.com",
+  storageBucket: "businesscontacts-77be3.appspot.com",
+  messagingSenderId: "243023914546"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
